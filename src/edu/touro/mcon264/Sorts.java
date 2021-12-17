8bd121c101e2e677bda73aba50dbdefb6132c71f
Jonathan Robinson <Jonathan.Robinson@turkprime.com>
2021-12-17 10:34:02 -0500

    Initial commit

---
+++ src/edu/touro/mcon264/Sorts.java
@@ -0,0 +1,23 @@
+package edu.touro.mcon264;
+
+public class Sorts
+{
+    public static <T extends Comparable<T> > void quickSort(T[] list) {
+        //// TODO
+    }
+
+    /*****
+     * returns the element with specified rank from the maximum. For example, if the <code>list</code> is
+     * <code>[4,5,8,11]</code> and <code>rank</code> is 0, the maximum value (11) will be returned. If rank is 1,2 or 3
+     * then 8, 5, or 4 will returned respectively.
+     * @param list
+     * @param rank
+     * @param <T>
+     * @return
+     */
+    public static <T extends Comparable<T> > T nthMax(T[] list, int rank) {
+//// TODO
+        return null;
+    }
+
+}
---
+++ tests/edu/touro/mcon264/SortsTest.java
@@ -0,0 +1,14 @@
+package edu.touro.mcon264;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortsTest {
+
+    @org.junit.jupiter.api.Test
+    void quickSort() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void nthMax() {
+    }
+}
